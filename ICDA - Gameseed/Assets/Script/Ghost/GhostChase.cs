6f1968a7471818582ab19928e0376557497e7406
NGnolep <goeinhoel@gmail.com>
2025-07-29 19:44:54 +0700

    added ghost sprite and animation

--- ICDA - Gameseed/Assets/Script/Ghost/GhostChase.cs
+++ ICDA - Gameseed/Assets/Script/Ghost/GhostChase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GhostChase : MonoBehaviour
@@ -16,17 +17,19 @@ public class GhostChase : MonoBehaviour
     private Vector2 roamTarget;
     private float roamTimer;
     public PlayerBar bar;
+    private Animator animator;
 
     public void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         PickNewRoamTarget();
     }
 
     public void Update()
     {
         float distance = Vector2.Distance(transform.position, player.position);
-
+        Vector2 moveDir = Vector2.zero;
         float alpha = Mathf.Clamp01(1 - (distance / fadeDistance));
         Color color = spriteRenderer.color;
         color.a = alpha;
@@ -35,8 +38,8 @@ public class GhostChase : MonoBehaviour
         if (distance < chaseDistance)
         {
             bar.isBeingChased = true;
-            Vector2 direction = (player.position - transform.position).normalized;
-            transform.position += (Vector3)(direction * speed * Time.deltaTime);
+            moveDir = (player.position - transform.position).normalized;
+            transform.position += (Vector3)(moveDir * speed * Time.deltaTime);
         }
         else
         {
@@ -47,8 +50,17 @@ public class GhostChase : MonoBehaviour
                 PickNewRoamTarget();
             }
 
-            Vector2 direction = (roamTarget - (Vector2)transform.position).normalized;
-            transform.position += (Vector3)(direction * speed * 0.5f * Time.deltaTime); // roam slower
+            moveDir = (roamTarget - (Vector2)transform.position).normalized;
+            transform.position += (Vector3)(moveDir * speed * 0.5f * Time.deltaTime); // roam slower
+        }
+
+        if (animator != null)
+        {
+            animator.SetBool("isMoving", moveDir.magnitude > 0.01f);
+        }
+        if (spriteRenderer != null && moveDir.x != 0)
+        {
+            spriteRenderer.flipX = moveDir.x < 0; 
         }
     }
 
